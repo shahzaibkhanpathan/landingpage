b76f5d778baee3e8687c811838929052e71307e0
shahzaibkhanpathan <168125979+shahzaibkhanpathan@users.noreply.github.com>
2024-11-13 17:01:27 +0500

    Initial commit for landingpage project

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Landing Page</title>
+  <!-- Bootstrap CSS -->
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+  <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+  <style>
+    body {
+        background-color: #d8fc92;
+    }
+    .navbar-brand {
+      color: #d3d3d3; 
+    }
+    .navbar-nav .nav-link {
+      color: #d3d3d3; 
+    }
+    .navbar-nav .nav-link:hover {
+      color: #ffffff; 
+    }
+    .navbar-brand:hover {
+      color: #ffffff; 
+    }
+    .btn-custom {
+      margin-left: 10px; 
+    }
+
+    .image {
+        width: 100%;
+        height:470px;
+    }
+  </style>
+</head>
+<body>
+    <div class="container-fluid-lg">
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Navbar Example</title>
+  <!-- Bootstrap CSS -->
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Publisher</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav mx-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        <div class="">
+            <a class="btn btn-outline-light btn-custom" href="#">Sign In</a>
+            <a class="btn btn-outline-light btn-custom" href="#">Sign Up</a>
+          </div>
+      </div>
+    </div>
+  </nav>
+      <section class="main">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2 class="mt-5 mb-4">
+                        Publisher ipsum dolor sit amet, <br>
+                         adipisicing elit. Nulla, eos.
+                   </h2>
+                   <span>
+                    Publisher ipsum dolor sit amet consectetur adipisicing elit.
+                     Voluptatibus ab reiciendis, incidunt aperiam magni pariatur dolore,
+                      ratione cum voluptate eaque sequi temporibus sed. 
+                      Quis ullam asperiores alias quas corporis iste nulla blanditiis,
+                       assumenda, quam minima perspiciatis aperiam tempora!
+                        Exercitationem eos est fugiat optio harum ullam reiciendis placeat et,
+                         corrupti perspiciatis.
+                    </span>
+                    <div class="input-group mt-5 mb-5">
+              <input type="search" class="form-control" placeholder="Your Email">
+              <button class="btn btn-dark">Subscribe <i class='far fa-arrow-alt-circle-right'></i></button>
+            </div>
+                </div>
+                <div class="col-md-6">
+                    <img src="img.avif" alt="" class="image mb-5">
+                </div>
+            </div>
+        </div>
+      </section>
+    </div>
+    <!-- Bootstrap JS and dependencies -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
